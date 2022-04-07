bc8d4150476277f66760b865a79054d1de5dc9f7
ShivamGoswami-16 <60355306+ShivamGoswami-16@users.noreply.github.com>
2022-04-07 12:30:19 +0530

    Update __init__.py

--- static/__init__.py
+++ static/__init__.py
@@ -15,7 +15,7 @@ logger = custom_logger(logger)
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/SampleDb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://mbkuesyyvswbyp:7d93d839e50512e7f724909e7d9aab8b0da3f42dc4207562cedce5730dccf45f@ec2-3-217-251-77.compute-1.amazonaws.com:5432/d8btsjep2p364r'
 
     from static.todoApp.model.todo_list_model import Todo
     db.init_app(app)
